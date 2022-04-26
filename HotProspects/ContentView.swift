029bb117e42c0ebfd42e13f5926174a7766fbc43
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-26 16:13:56 +0400

    contextMenu example

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,37 +7,33 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
-    @State private var output = ""
+    @State private var backgroundColor = Color.red
     
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-    
-    func fetchReadings() async {
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        let result = await fetchTask.result
-        
-//        do {
-//            output = try result.get()
-//        }catch{
-//            print("Download error")
-//        }
-        switch result{
-        case .success(let srt):
-            output = srt
-        case .failure(let error):
-            output = "Download error"
+        VStack{
+            Text("Hello, world!")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button() {
+                        backgroundColor = .red
+                    } label: {
+                        Label("Red", systemImage: "checkmark.circle.fill")
+                            .foregroundColor(.red)
+                    }
+                    
+                    Button("Green") {
+                        backgroundColor = .green
+                    }
+                    
+                    Button("Blue") {
+                        backgroundColor = .blue
+                    }
+                }
         }
     }
 }
