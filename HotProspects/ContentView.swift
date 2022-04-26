91eee96a3cd36ba5a0f84db9b3542bceaec49315
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-26 17:43:15 +0400

    swipe actions explained

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,31 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
     
     var body: some View {
-        VStack{
-            Text("Hello, world!")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change Color")
-                .padding()
-                .contextMenu {
-                    Button() {
-                        backgroundColor = .red
+        List{
+            Text("Bob the builder")
+                .swipeActions {
+                    Button(role: .destructive) {
+                        print("deleting")
                     } label: {
-                        Label("Red", systemImage: "checkmark.circle.fill")
-                            .foregroundColor(.red)
+                        Label("Delete", systemImage: "minus.circle")
                     }
-                    
-                    Button("Green") {
-                        backgroundColor = .green
+                }
+                .swipeActions(edge: .leading){
+                    Button{
+                        print("Pinning")
+                    } label: {
+                        Label("Pin",systemImage: "pin")
                     }
-                    
-                    Button("Blue") {
-                        backgroundColor = .blue
-                    }
+                    .tint(.orange)
                 }
         }
     }
