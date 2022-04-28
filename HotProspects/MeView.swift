9505206e7abcab17d894e46c52b0209a84bee65e
Evgenii Shatokhin <shatokhin.e@northeastern.edu>
2022-04-28 20:44:44 +0400

    saving the qr code from the context menu

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -12,6 +12,7 @@ import CoreImage
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@yoursite.com"
+    @State private var QRCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -27,21 +28,36 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: QRCode)
                     .resizable()
                     .interpolation(.none)
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: QRCode)
+                                                       
+                        } label: {
+                            Label("Save my QR",systemImage: "square.and.arrow.down")
+                        }
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateQRCode)
+            .onChange(of: name) { _ in updateQRCode() }
+            .onChange(of: emailAddress) { _ in updateQRCode() }
         }
+    }
+    func updateQRCode() {
+        QRCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
         filter.message  = Data(string.utf8)
         if let outputImage = filter.outputImage {
-            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent)
-            {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                
                 return UIImage(cgImage: cgimg)
             }
         }
